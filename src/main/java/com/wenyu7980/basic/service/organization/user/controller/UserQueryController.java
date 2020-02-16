4d39879adf51e2fcb53398c2f43fd63ad495bb07
wenyu <wenyu7980@163.com>
2020-02-16 11:10:24 +0800

    用户查询

--- src/main/java/com/wenyu7980/basic/service/organization/user/controller/UserQueryController.java
+++ src/main/java/com/wenyu7980/basic/service/organization/user/controller/UserQueryController.java
@@ -8,16 +8,14 @@ import com.wenyu7980.basic.common.query.QuerySearchUtil;
 import com.wenyu7980.basic.service.organization.user.domain.UserDetail;
 import com.wenyu7980.basic.service.organization.user.domain.UserListDetail;
 import com.wenyu7980.basic.service.organization.user.handler.impl.UserQueryHandler;
-import com.wenyu7980.query.QueryCompare;
-import com.wenyu7980.query.QueryCondition;
-import com.wenyu7980.query.QueryLogic;
-import com.wenyu7980.query.QueryPredicateExpress;
-import io.swagger.annotations.*;
+import com.wenyu7980.query.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -35,11 +33,7 @@ public class UserQueryController {
     private UserQueryHandler queryHandler;
 
     @ApiOperation("用户查询")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "查询成功"),
-            @ApiResponse(code = 404, message = "不存在")
-    })
-    @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "{id}")
     @ResponseStatus(code = HttpStatus.OK)
     public UserDetail getUser(@ApiParam("用户id") @PathVariable("id") String id,
             @ApiParam("是否查询详情") @RequestParam(value = "detail", defaultValue = "false") boolean detail) {
@@ -47,25 +41,24 @@ public class UserQueryController {
     }
 
     @ApiOperation("用户列表查询")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "查询成功")
-    })
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping()
     @ResponseStatus(code = HttpStatus.OK)
     public PageBody<UserListDetail> getUsers(
+            @ApiParam("所属部门id") @RequestParam(value = "departmentId", required = false) String departmentId,
+            @ApiParam("所属管理部门id") @RequestParam(value = "adminDepartmentId", required = false) String adminDepartmentId,
             @ApiParam("页码") @RequestParam(value = "index", defaultValue = "0") Integer index,
             @ApiParam("页大小") @RequestParam(value = "size", defaultValue = "20") Integer size,
             @ApiParam("是否查询详情") @RequestParam(value = "detail", defaultValue = "false") boolean detail) {
-        return queryHandler.getPage(QueryLogic.and(), PageRequest
-                        .of(index, size, Sort.Direction.DESC, "updatedDateTime"),
-                detail);
+        return queryHandler.getPage(QueryLogic.and(QueryJoin.join("departments",
+                QueryCondition.of("id", QueryCompare.EQ, departmentId)),
+                QueryJoin.join("adminDepartments", QueryCondition
+                        .of("id", QueryCompare.EQ, adminDepartmentId))),
+                PageRequest.of(index, size, Sort.Direction.DESC,
+                        "updatedDateTime"), detail);
     }
 
     @ApiOperation("用户列表查询")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "查询成功")
-    })
-    @PostMapping(path = "search", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "search")
     @ResponseStatus(code = HttpStatus.OK)
     @AuthRequest(required = false)
     public PageBody<UserListDetail> searchUsers(
@@ -85,7 +78,7 @@ public class UserQueryController {
             @Override
             public QueryPredicateExpress queryExpress(QueryCompare compare,
                     Object object) {
-                return QueryCondition.of("username", compare,
+                return QueryCondition. of("username", compare,
                         object == null ? null : object.toString());
             }
         };
